24bf7af8bbca27552542ac7527846e4ec5118e15
ashima1491 <sharma.ashima91@gmail.com>
2019-04-04 17:10:52 -0500

    login/signUp added

--- src/main/java/edu/tamu/istm631/team17/controller/LoginController.java
+++ src/main/java/edu/tamu/istm631/team17/controller/LoginController.java
@@ -9,16 +9,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import edu.tamu.istm631.team17.models.User;
 import edu.tamu.istm631.team17.repo.UserRepo;
 
 
 
-@Controller    
+
+@RestController    
 @RequestMapping(path="/login") 
 public class LoginController
 {
+
 	@Autowired 
 	private UserRepo userRepo;
 
@@ -30,8 +33,9 @@ public class LoginController
 		/*User n = new User();
 		n.setName(name);
 		n.setEmail(email);*/
+		System.out.println(user.getFname()+" hii ");
 		userRepo.save(user);
-		return "Saved";
+		return "true";
 	}
 
 	@GetMapping(path="/all")
@@ -45,5 +49,19 @@ public class LoginController
 		// This returns a JSON or XML with the users
 		return userRepo.findByemailid(emailid);
 	}
+	
+	@PostMapping(path="/authenticate") 
+	public User authenticate (@RequestBody User user) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+
+		/*User n = new User();
+		n.setName(name);
+		n.setEmail(email);*/
+		System.out.println(user.getEmailid()+" hii "+user.getPassword());
+		return userRepo.authenticate(user.getEmailid(), user.getPassword());
+		
+	}
+	
 	
 }
--- src/main/java/edu/tamu/istm631/team17/repo/UserRepo.java
+++ src/main/java/edu/tamu/istm631/team17/repo/UserRepo.java
@@ -2,7 +2,9 @@ package edu.tamu.istm631.team17.repo;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import edu.tamu.istm631.team17.models.User;
 
@@ -10,5 +12,8 @@ public interface UserRepo extends CrudRepository<User, Integer>
 {
 
     List<User> findByemailid(String emailid);
+    
+    @Query("SELECT u FROM User u WHERE LOWER(u.emailid) = LOWER(:emailid) and u.password = :password")
+    public User authenticate(@Param("emailid") String emailid, @Param("password") String password);
 
 }
