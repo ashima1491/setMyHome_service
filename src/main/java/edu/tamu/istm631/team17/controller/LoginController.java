0e8fc0dadc25f160d61ad9a1259d3bb2bd8ce77a
ashima1491 <sharma.ashima91@gmail.com>
2019-04-06 22:28:14 -0500

    cross origin header added to test on heroku

--- src/main/java/edu/tamu/istm631/team17/controller/LoginController.java
+++ src/main/java/edu/tamu/istm631/team17/controller/LoginController.java
@@ -3,6 +3,7 @@ package edu.tamu.istm631.team17.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,12 +40,14 @@ public class LoginController
 	}
 
 	@GetMapping(path="/all")
+	@CrossOrigin(origins = "https://setmyhome.herokuapp.com/")
 	public @ResponseBody Iterable<Person> getAllUsers() {
 		// This returns a JSON or XML with the users
 		return userRepo.findAll();
 	}
 	
 	@GetMapping(path="/test")
+	@CrossOrigin(origins = "https://setmyhome.herokuapp.com/")
 	public String test() {
 		// This returns a JSON or XML with the users
 		return "working!";
@@ -58,6 +61,7 @@ public class LoginController
 	}
 	
 	@PostMapping(path="/authenticate") 
+	@CrossOrigin(origins = "https://setmyhome.herokuapp.com/")
 	public Person authenticate (@RequestBody Person user) {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
